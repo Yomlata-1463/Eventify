8e52dd882753a79811a8139503e2bac75e9a9cf7
Yomlata Assefa <yomlata.assefa@bitscollege.edu.et>
2025-06-23 21:50:43 +0300

    add home files

--- home-script.js
+++ home-script.js
@@ -3,42 +3,103 @@ const home = document.getElementById('home-nav');
 const add_event = document.getElementById('addevent-nav');
 const my_reservations = document.getElementById('myreservations-nav');
 const logout = document.getElementById('logout-nav');
+const mobile_home = document.getElementById('mobile-home-nav');
+const mobile_addevent = document.getElementById('mobile-add-nav');
+const mobile_myreservations = document.getElementById('mobile-myreservation-nav');
+const mobile_logout = document.getElementById('mobile-logout-nav');
+const my_reservations_section = document.getElementById('my-reservations-section');
+const content = document.getElementById('content');
+const hr = document.getElementById('my-reservations-hr');
+const title = document.querySelector('h2');
+
+let navLocked = false;
 
 if (home) {
     home.onclick = function () {
+         if (navLocked) {
+            alert("Please submit the event or close the form before navigating.");
+            return;
+        }
+        my_reservations_section.style.display = 'none'
+        content.style.display = 'flex'
+        hr.style.display = 'none'
+        title.innerHTML = 'UPCOMING EVENTS';
         home.style.backgroundColor = '#B8CFCE';
         add_event.style.backgroundColor = '#7F8CAA';
         my_reservations.style.backgroundColor = '#7F8CAA';
-        logout.style.backgroundColor = '#7F8CAA';
+    };
+}
+
+if (mobile_home) {
+    mobile_home.onclick = function () {
+         if (navLocked) {
+            alert("Please submit the event or close the form before navigating.");
+            return;
+        }
     };
 }
 
 if (add_event) {
     add_event.onclick = function () {
+        add_section.style.display = 'block';
         add_event.style.backgroundColor = '#B8CFCE';
         home.style.backgroundColor = '#7F8CAA';
         my_reservations.style.backgroundColor = '#7F8CAA';
-        logout.style.backgroundColor = '#7F8CAA';
+        navLocked = true;
+    };
+}
+
+if (mobile_addevent) {
+    mobile_addevent.onclick = function () {
+        add_section.style.display = 'block';
+        add_event.style.backgroundColor = '#B8CFCE';
+        home.style.backgroundColor = '#7F8CAA';
+        my_reservations.style.backgroundColor = '#7F8CAA';
+        navLocked = true;
     };
 }
 
 if (my_reservations) {
     my_reservations.onclick = function () {
+         if (navLocked) {
+            alert("Please submit the event or close the form before navigating.");
+            return;
+        }
+        content.style.display = 'none'
+        my_reservations_section.style.display = 'flex'
+        hr.style.display = 'block'
+        title.innerHTML = 'MY RESERVATIONS';
         my_reservations.style.backgroundColor = '#B8CFCE';
         home.style.backgroundColor = '#7F8CAA';
         add_event.style.backgroundColor = '#7F8CAA';
-        logout.style.backgroundColor = '#7F8CAA';
     };
 }
 
 if (logout) {
     logout.onclick = function () {
+        if (navLocked) {
+            alert("Please submit the event or close the form before navigating.");
+            return;
+        }
         if (confirm("Are you sure you want to logout?")) {
             location.href = "login.html";
         } else {
             history.back();
         }
-        
+    };
+}
+
+if (mobile_logout) {
+    mobile_logout.onclick = function () {
+        if (navLocked) {
+            alert("Please submit the event or close the form before navigating.");
+            return;
+        }
+        if (confirm("Are you sure you want to logout?")) {
+            location.href = "login.html";
+        } else {
+            history.back();
+        }
     };
 }
 
@@ -99,4 +160,18 @@ if (mobile_logout_Btn) {
     }
 }
 
+
+
+const close_add = document.getElementById('close-btn');
+const add_section = document.getElementById('add-event-section')
+
+if (close_add) {
+    close_add.onclick = function () {
+        add_section.style.display = 'none';
+        home.style.backgroundColor = '#B8CFCE';
+        add_event.style.backgroundColor = '#7F8CAA';
+        my_reservations.style.backgroundColor = '#7F8CAA';
+        navLocked = false;
+    };
+}
 
